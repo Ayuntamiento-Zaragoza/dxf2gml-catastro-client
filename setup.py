b8e6bb248b4a7ed8e8ee5a5770016809d3132686
Jose Antonio Chavarría <jachavar@gmail.com>
2023-04-19 09:19:45 +0200

    feat: python upgrade (version 3)

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2016 Jose Antonio Chavarría <jachavar@gmail.com>
-#                    Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2016-2023 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2016-2023 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,13 +21,17 @@ __license__ = 'GPLv3'
 
 import sys
 
-if not hasattr(sys, 'version_info') or sys.version_info < (2, 7, 0, 'final'):
-    raise SystemExit('This project requires Python 2.7 or later.')
+if not hasattr(sys, 'version_info') or sys.version_info < (3, 6, 0, 'final'):
+    raise SystemExit('This project requires Python 3.6 or later.')
 
 import os
 PATH = os.path.dirname(__file__)
 README = open(os.path.join(PATH, 'README.md')).read()
 VERSION = open(os.path.join(PATH, 'VERSION')).read().splitlines()[0]
+REQUIRES = filter(
+    lambda s: len(s) > 0,
+    open(os.path.join(PATH, 'requirements.txt'), encoding='utf_8').read().split('\n')
+)
 
 from setuptools import setup, find_packages
 
@@ -36,14 +39,23 @@ from setuptools import setup, find_packages
 setup(
     name='dxf2gml-catastro-client',
     version=VERSION,
-    description='dxf2gml_catastro_client  to generate GML files from DXF',
+    description='dxf2gml_catastro_client to generate GML files from DXF',
     long_description=README,
     license='GPLv3',
     author='Alberto Gacías',
     author_email='alberto@migasfree.org',
     platforms=['Linux'],
+    install_requires=REQUIRES,
     packages = find_packages(),
     scripts=['bin/dxf2gml-catastro'],
+    data_files=[
+        ('share/doc/dxf2gml-catastro-client', [
+            'AUTHORS',
+            'INSTALL',
+            'LICENSE',
+            'README.md',
+        ]),
+    ],
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 4 - Beta',
